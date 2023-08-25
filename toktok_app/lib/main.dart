1e9cdc41ee7560a70f2e0dc3f9d04f6a98b3e6df
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-08-26 02:25:33 +0800

    New Changes :
    feat(CustomBottomNav)

--- toktok_app/lib/main.dart
+++ toktok_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:toktok_app/navigation_container.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Toktok Clone App',
+      theme: ThemeData(
+        textTheme: GoogleFonts.varelaRoundTextTheme(),
       ),
+      home: NavigationContainer(),
     );
   }
 }
