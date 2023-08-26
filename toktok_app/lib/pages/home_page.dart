7978bd404995cd5d3fb527388631ea1f959e641e
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-08-26 18:04:02 +0800

    New Changes :
    - Fix icon not appear
    - Adding tab following and For You
    - Side Icon Item

--- toktok_app/lib/pages/home_page.dart
+++ toktok_app/lib/pages/home_page.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import '../widgets/home_side_bar.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isFollowingSelected = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
-    );
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: () => {
+                  setState(() {
+                    _isFollowingSelected = true;
+                  })
+                },
+                child: Text(
+                  "Following",
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: _isFollowingSelected ? 18 : 15,
+                      color: _isFollowingSelected ? Colors.white : Colors.grey),
+                ),
+              ),
+              Text(
+                "   |   ",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(fontSize: 15, color: Colors.grey),
+              ),
+              GestureDetector(
+                onTap: () => {
+                  setState(() {
+                    _isFollowingSelected = false;
+                  })
+                },
+                child: Text(
+                  "For you",
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: !_isFollowingSelected ? 18 : 15,
+                      color:
+                          !_isFollowingSelected ? Colors.white : Colors.grey),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: PageView.builder(
+            onPageChanged: (int page) => {},
+            scrollDirection: Axis.vertical,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    color: Colors.amber,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.purple,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height / 1.75,
+                          color: Colors.pink,
+                          child: HomeSideBar(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }));
   }
 }
---
+++ toktok_app/lib/widgets/home_side_bar.dart
@@ -0,0 +1,113 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeSideBar extends StatefulWidget {
+  const HomeSideBar({super.key});
+
+  @override
+  State<HomeSideBar> createState() => _HomeSideBarState();
+}
+
+class _HomeSideBarState extends State<HomeSideBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+    _animationController.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle style = Theme.of(context)
+        .textTheme
+        .bodyText1!
+        .copyWith(fontSize: 13, color: Colors.white);
+
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          _profileImageButton(),
+          _sideBarItem('heart', '1.2M', style),
+          _sideBarItem('comment', '123', style),
+          _sideBarItem('share', 'Share', style),
+          AnimatedBuilder(
+              animation: _animationController,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset('assets/disc.png'),
+                  )
+                ],
+              ),
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: 2 * pi * _animationController.value,
+                  child: child,
+                );
+              })
+        ],
+      ),
+    );
+  }
+
+  _sideBarItem(String iconName, String label, TextStyle style) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/$iconName.svg'),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(label, style: style)
+      ],
+    );
+  }
+
+  _profileImageButton() {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(24),
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://instagram.fkul3-4.fna.fbcdn.net/v/t51.2885-19/309338404_493944315918418_3470607335458966523_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fkul3-4.fna.fbcdn.net&_nc_cat=110&_nc_ohc=rs0lLZ3qzXwAX_-x-sL&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAHyzX1fALQzt5-1yPddzVM7DV2tyR_Vw-FyV0Ily3t4w&oe=64EE0B78&_nc_sid=8b3546'))),
+        ),
+        Positioned(
+            bottom: -10,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red, borderRadius: BorderRadius.circular(25)),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 20,
+              ),
+            ))
+      ],
+    );
+  }
+}
